77598c51ab5847c342e4f00d5f85298d549b0137
Renard Bleu <renardbleu84100@gmail.com>
2025-05-19 11:10:26 +0200

    add : backend show page produit

--- src/controleur/produitControleur.php
+++ src/controleur/produitControleur.php
@@ -2,5 +2,32 @@
 
 function ShowProduitControleur($twig, $db){
     $form = array();
-    echo $twig ->render('showProduit.twig', array());
+    $id = $_GET['id'];
+    $form['valide'] = true;
+    
+    if(isset($id)){
+        $produit = new Produit($db);
+        $unProduit = $produit->produit($id);
+
+        if(isset($unProduit)){
+            if($unProduit['deleteAt'] == null){
+                $form['produit'] = $unProduit;
+
+                if(isset($form['produit']['caracteristiques'])){
+                    $caracteristiques = explode('//', $form['produit']['caracteristiques']);
+                    foreach ($caracteristiques as $caracteristique){
+                        $caracteristique = trim($caracteristique);
+                        $form['caracteristiques'][] = $caracteristique;
+                    }
+                }
+            }else{
+                $form['message'] = 'le produit que vous voulez voir n\'est plus disponible';
+            }
+        }else{
+            $form['message'] = 'le produit que vous voulez voir n\'existe pas';
+        }
+    }else{
+        $form['message'] = 'id du produit non renseigné';
+    }
+    echo $twig ->render('showProduit.twig', array('form'=>$form,'liste'=>$liste));
 }
